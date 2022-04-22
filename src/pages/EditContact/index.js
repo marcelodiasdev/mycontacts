dc692326cd212a88eb3a98b0a534dc026a364354
Marcelo Dias <marcelo.dias@redspark.io>
2022-04-22 20:35:51 -0300

    add component header

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,3 +1,9 @@
+import PageHeader from '../../components/PageHeader';
+
 export default function EditContact() {
-  return 'edit';
+  return (
+    <PageHeader
+      title="Editar Mateus Silva"
+    />
+  );
 }
