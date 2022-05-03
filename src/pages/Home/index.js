41d83ca2db2c833a668f54681f6cb3a39de1c527
Marcelo Dias <marcelo.dias@redspark.io>
2022-05-03 09:11:04 -0300

    add component ContactForm

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import {
   Container, InputSearchContainer, Header, ListContainer, Card,
 } from './styles';
@@ -15,7 +17,7 @@ export default function Home() {
       </InputSearchContainer>
       <Header>
         <strong> 3 contatos </strong>
-        <a href="/"> Novo contato</a>
+        <Link to="/new"> Novo contato</Link>
       </Header>
 
       <ListContainer>
@@ -34,53 +36,17 @@ export default function Home() {
             <span>devoficial@email.com</span>
             <span>(11) 9999-9999</span>
           </div>
+
           <div className="actions">
-            <a href="/">
+            <Link to="/edit/123">
               <img src={edit} alt="Edit" />
-            </a>
+            </Link>
             <button type="button">
               <img src={trash} alt="Delete" />
             </button>
           </div>
         </Card>
 
-        <Card>
-          <div className="info">
-            <div className="contact-name">
-              <strong>Dev Oficial</strong>
-              <small>instagram</small>
-            </div>
-            <span>devoficial@email.com</span>
-            <span>(11) 9999-9999</span>
-          </div>
-          <div className="actions">
-            <a href="/">
-              <img src={edit} alt="Edit" />
-            </a>
-            <button type="button">
-              <img src={trash} alt="Delete" />
-            </button>
-          </div>
-        </Card>
-
-        <Card>
-          <div className="info">
-            <div className="contact-name">
-              <strong>Dev Oficial</strong>
-              <small>instagram</small>
-            </div>
-            <span>devoficial@email.com</span>
-            <span>(11) 9999-9999</span>
-          </div>
-          <div className="actions">
-            <a href="/">
-              <img src={edit} alt="Edit" />
-            </a>
-            <button type="button">
-              <img src={trash} alt="Delete" />
-            </button>
-          </div>
-        </Card>
       </ListContainer>
     </Container>
   );
